c3e366da0325761c19a84264158296a79e3c4eaf
Colin Catlin <colin.catlin@gmail.com>
2019-12-23 23:46:53 -0600

    Witcher S1E3

--- autots/evaluator/auto_model.py
+++ autots/evaluator/auto_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 
 class ModelObject(object):
     """
@@ -9,13 +10,52 @@ class ModelObject(object):
     
     Args:
         name (str): Model Name
+        frequency (str): String alias of datetime index frequency or else 'infer'
+        prediction_interval (float): Confidence interval for probabilistic forecast
     """
-    def __init__(self, name: str = "Uniniated Model Name"):
+    def __init__(self, name: str = "Uniniated Model Name", frequency: str = 'infer', prediction_interval: float = 0.9, fit_runtime=datetime.timedelta(0)):
         self.name = name
+        self.frequency = frequency
+        self.prediction_interval = prediction_interval
+        self.fit_runtime = fit_runtime
     
     def __repr__(self):
-        return self.name
+        return 'ModelObject of ' + self.name
     
+    def basic_profile(self, df):
+        self.startTime = datetime.datetime.now()
+        self.train_shape = df.shape
+        self.column_names = df.columns
+        self.train_last_date = df.index[-1]
+        if self.frequency == 'infer':
+            self.frequency = pd.infer_freq(df.index, warn = False)
+        
+        return df
+    
+    def create_forecast_index(self, forecast_length: int):
+        """
+        Requires ModelObject.basic_profile() being called as part of .fit()
+        """
+        forecast_index = pd.date_range(freq = self.frequency, start = self.train_last_date, periods = forecast_length + 1)
+        forecast_index = forecast_index[1:]
+        self.forecast_index = forecast_index
+        return forecast_index
+    
+    def get_new_params(method: str = 'random'):
+        """Returns dict of new parameters for parameter tuning
+        """
+        return {}
+
+class PredictionObject(object):
+    def __init__(self, forecast_length: int = 0, lower_forecast = np.nan, forecast = np.nan, upper_forecast = np.nan, prediction_interval: float = 0.9, predict_runtime=datetime.timedelta(0)):
+        self.forecast_length = forecast_length
+        self.lower_forecast = lower_forecast
+        self.forecast = forecast
+        self.upper_forecast = upper_forecast
+        self.prediction_interval = prediction_interval
+        self.predict_runtime = predict_runtime
+
+
 def ModelMonster(model: str, parameters: dict):
     """Directs strings and parameters to appropriate model objects.
     
@@ -23,3 +63,6 @@ def ModelMonster(model: str, parameters: dict):
         model (str): Name of Model Function
         parameters (dict): Dictionary of parameters to pass through to model
     """
+    if model = 'Zeroes':
+        from autots.models.basics import Zeroes
+        return Zeroes()
--- autots/models/basics.py
+++ autots/models/basics.py
@@ -1,66 +1,115 @@
 """
 Naives and Others Requiring No Additional Packages Beyond Numpy and Pandas
 """
+import datetime
 import numpy as np
 import pandas as pd
 from autots.evaluator.auto_model import ModelObject
+from autots.evaluator.auto_model import PredictionObject
 
 
 class Zeroes(ModelObject):
-    def __init__(self, name):
-        ModelObject.__init__(self, name)
+    """Naive forecasting predicting a dataframe of zeroes (0's)
+    
+    Args:
+        name (str): String to identify class
+        frequency (str): String alias of datetime index frequency or else 'infer'
+        prediction_interval (float): Confidence interval for probabilistic forecast
+
+    """
+    def __init__(self, name: str = "Zeroes", frequency: str = 'infer', prediction_interval: float = 0.9):
+        ModelObject.__init__(self, name, frequency, prediction_interval)
     def fit(self, df):
-        self.startTime = datetime.datetime.now()
-        self.shape = df.shape
+        """Train algorithm given data supplied 
+        
+        Args:
+            df (pandas.DataFrame): Datetime Indexed 
+        """
+        df = self.basic_profile(df)
+        self.fit_runtime = datetime.datetime.now() - self.startTime
         return self
-    def predict(self, forecast_length, regressor):
+
+    def predict(self, forecast_length: int, regressor = [], just_point_forecast = False):
+        """Generates forecast data immediately following dates of index supplied to .fit()
         
-        return df
+        Args:
+            forecast_length (int): Number of periods of data to forecast ahead
+            regressor (numpy.Array): additional regressor, not used
+            just_point_forecast (bool): If True, return a pandas.DataFrame of just point forecasts
+            
+        Returns:
+            Either a PredictionObject of forecasts and metadata, or
+            if just_point_forecast == True, a dataframe of point forecasts
+        """
+        predictStartTime = datetime.datetime.now()
+        df = pd.DataFrame(np.zeros((forecast_length,(self.train_shape[1]))), columns = self.column_names, index = self.create_forecast_index(forecast_length=forecast_length) )
+        if just_point_forecast:
+            return df
+        else:
+            predict_runtime = datetime.datetime.now() - predictStartTime
+            prediction = PredictionObject(forecast_length=forecast_length,lower_forecast=df,
+                                          forecast=df, upper_forecast=df,
+                                          prediction_interval=self.prediction_interval,
+                                          predict_runtime=predict_runtime)
+            
+            return prediction
+    def get_new_params(method: str = 'random'):
+        """Returns dict of new parameters for parameter tuning
+        """
+        return {}
+
+class LastValue(ModelObject):
+    """Naive forecasting predicting a dataframe of the last series value
+    
+    Args:
+        name (str): String to identify class
+        frequency (str): String alias of datetime index frequency or else 'infer'
+        prediction_interval (float): Confidence interval for probabilistic forecast
+
+    """
+    def __init__(self, name: str = "LastValue", frequency: str = 'infer', prediction_interval: float = 0.9):
+        ModelObject.__init__(self, name, frequency, prediction_interval)
+    def fit(self, df):
+        """Train algorithm given data supplied 
         
-try:
-    startTime = datetime.datetime.now()
-    Zeroes.forecast = (np.zeros((forecast_length,len(train.columns))))
-    Zeroes.runtime = datetime.datetime.now() - startTime
-    
-    Zeroes.mae = pd.DataFrame(mae(test.values, Zeroes.forecast)).mean(axis=0, skipna = True)
-    Zeroes.overall_mae = np.nanmean(Zeroes.mae)
-    Zeroes.smape = smape(test.values, Zeroes.forecast)
-    Zeroes.overall_smape = np.nanmean(Zeroes.smape)
-except Exception as e:
-    print(e)
-    error_list.extend([traceback.format_exc()])
+        Args:
+            df (pandas.DataFrame): Datetime Indexed 
+        """
+        df = self.basic_profile(df)
+        self.last_values = df.tail(1).values
+        self.fit_runtime = datetime.datetime.now() - self.startTime
+        return self
 
-currentResult = pd.DataFrame({
-        'method': Zeroes.name, 
-        'runtime': Zeroes.runtime, 
-        'overall_smape': Zeroes.overall_smape, 
-        'overall_mae': Zeroes.overall_mae,
-        'object_name': 'Zeroes'
-        }, index = [0])
-model_results = pd.concat([model_results, currentResult], ignore_index = True).reset_index(drop = True)
+    def predict(self, forecast_length: int, regressor = [], just_point_forecast = False):
+        """Generates forecast data immediately following dates of index supplied to .fit()
+        
+        Args:
+            forecast_length (int): Number of periods of data to forecast ahead
+            regressor (numpy.Array): additional regressor, not used
+            just_point_forecast (bool): If True, return a pandas.DataFrame of just point forecasts
+            
+        Returns:
+            Either a PredictionObject of forecasts and metadata, or
+            if just_point_forecast == True, a dataframe of point forecasts
+        """
+        predictStartTime = datetime.datetime.now()
+        df = pd.DataFrame(np.tile(self.last_values, (forecast_length,1)))
+        df = pd.DataFrame(np.zeros((forecast_length,(self.train_shape[1]))), columns = self.column_names, index = self.create_forecast_index(forecast_length=forecast_length) )
+        if just_point_forecast:
+            return df
+        else:
+            predict_runtime = datetime.datetime.now() - predictStartTime
+            prediction = PredictionObject(forecast_length=forecast_length,lower_forecast=df,
+                                          forecast=df, upper_forecast=df,
+                                          prediction_interval=self.prediction_interval,
+                                          predict_runtime=predict_runtime)
+            
+            return prediction
+    def get_new_params(method: str = 'random'):
+        """Returns dict of new parameters for parameter tuning
+        """
+        return {}
 
-LastValue = ModelResult("Last Value Naive")
-try:
-    startTime = datetime.datetime.now()
-    LastValue.forecast = np.tile(train.tail(1).values, (forecast_length,1))
-    LastValue.runtime = datetime.datetime.now() - startTime
-    
-    LastValue.mae = pd.DataFrame(mae(test.values, LastValue.forecast)).mean(axis=0, skipna = True)
-    LastValue.overall_mae = np.nanmean(LastValue.mae)
-    LastValue.smape = smape(test.values, LastValue.forecast)
-    LastValue.overall_smape = np.nanmean(LastValue.smape)
-except Exception as e:
-    print(e)
-    error_list.extend([traceback.format_exc()])
-
-currentResult = pd.DataFrame({
-        'method': LastValue.name, 
-        'runtime': LastValue.runtime, 
-        'overall_smape': LastValue.overall_smape, 
-        'overall_mae': LastValue.overall_mae,
-        'object_name': 'LastValue'
-        }, index = [0])
-model_results = pd.concat([model_results, currentResult], ignore_index = True).reset_index(drop = True)
 
 MedValue = ModelResult("Median Naive")
 try:
