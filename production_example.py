0b1c3dd41424cfff01db72aaa6df2a4b963c7f89
Colin Catlin <colin.catlin@gmail.com>
2022-03-31 22:32:52 -0500

    0.4.1 a5

--- production_example.py
+++ production_example.py
@@ -20,6 +20,7 @@ import matplotlib.pyplot as plt  # required only for graphs
 from autots import AutoTS, load_live_daily, create_regressor
 
 fred_key = None  # https://fred.stlouisfed.org/docs/api/api_key.html
+gsa_key = None
 forecast_name = "example"
 graph = True  # whether to plot graphs
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects
