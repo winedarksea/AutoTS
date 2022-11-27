5a46cae72b2c7c80334a33e1c8dd9976e7e5f453
Colin Catlin <colin.catlin@gmail.com>
2022-11-27 17:44:38 -0600

    0.5.2 a4

--- autots/evaluator/auto_ts.py
+++ autots/evaluator/auto_ts.py
@@ -552,7 +552,7 @@ class AutoTS(object):
             'num_validations': random.choice([0, 1, 2, 3, 4, 6]),
             'validation_method': random.choices(
                 ['backwards', 'even', 'similarity', 'seasonal 364', 'seasonal'],
-                [0.4, 0.1, 0.3, 0.3],
+                [0.4, 0.1, 0.3, 0.3, 0.2],
             )[0],
             'models_to_validate': random.choices(
                 [0.15, 0.10, 0.25, 0.35, 0.45], [0.3, 0.1, 0.3, 0.3, 0.1]
