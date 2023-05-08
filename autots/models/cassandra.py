beabcbe58a2cbef6b499bda097da09cd5125ee03
Colin Catlin <colin.catlin@gmail.com>
2023-05-08 10:58:30 -0500

    0.5.7 a6

--- autots/models/cassandra.py
+++ autots/models/cassandra.py
@@ -1751,8 +1751,9 @@ class Cassandra(ModelObject):
                     'UnivariateMotif',
                     'UnobservedComponents',
                     "KalmanStateSpace",
+                    'RRVAR',
                 ],
-                [0.05, 0.05, 0.1, 0.05, 0.05, 0.15, 0.05, 0.05, 0.05, 0.05],
+                [0.05, 0.05, 0.1, 0.05, 0.05, 0.15, 0.05, 0.05, 0.05, 0.05, 0.05],
                 k=1,
             )[0]
             trend_model = {'Model': model_str}
