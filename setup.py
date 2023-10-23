c026f23d53814473eea271226d9906fda5b3765b
Colin Catlin <colin.catlin@gmail.com>
2023-10-23 09:49:28 -0500

    0.6.2 a30

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoTS",
-    version="0.6.1",
+    version="0.6.2",
     author="Colin Catlin",
     author_email="colin.catlin@syllepsis.live",
     description="Automated Time Series Forecasting",
