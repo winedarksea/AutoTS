5e0ea06a0cc582c94b4e5930d4ac98fd64121aea
Colin Catlin <colin.catlin@gmail.com>
2024-05-06 08:17:33 -0500

    0.6.12

--- autots/__init__.py
+++ autots/__init__.py
@@ -27,7 +27,7 @@ from autots.evaluator.anomaly_detector import AnomalyDetector, HolidayDetector
 from autots.models.cassandra import Cassandra
 
 
-__version__ = '0.6.11'
+__version__ = '0.6.12'
 
 TransformTS = GeneralTransformer
 
