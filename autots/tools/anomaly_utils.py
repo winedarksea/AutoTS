73eebbde367f6056cb9503e95def371c30f50cf1
Colin Catlin <colin.catlin@gmail.com>
2024-08-27 20:42:53 -0500

    0.6.16 a12

--- autots/tools/anomaly_utils.py
+++ autots/tools/anomaly_utils.py
@@ -474,7 +474,7 @@ def anomaly_new_params(method='random'):
     if method == "deep":
         method_choice = random.choices(
             available_methods,
-            [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05, 0.1, 0.1, 0.15, 0.1],
+            [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05, 0.1, 0.1, 0.15, 0.1, 0.1, 0.1, 0.1],
         )[0]
     elif method == "fast":
         method_choice = random.choices(
