ee6d21e379bb24d65a31c33c5ec7f5ee1428c01f
Colin Catlin <colin.catlin@gmail.com>
2024-12-09 08:40:44 -0600

    0.6.18 a3

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
