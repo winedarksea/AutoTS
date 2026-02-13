0da130c0ac189742a4df85667f4337d032170a85
Colin Catlin <colin.catlin@gmail.com>
2026-02-12 23:12:47 -0600

    1.0.2 a15

--- autots/evaluator/auto_model.py
+++ autots/evaluator/auto_model.py
@@ -892,8 +892,6 @@ class ModelPrediction(ModelObject):
 
         self.transformation_runtime = datetime.datetime.now() - transformationStartTime
         # from autots.evaluator.auto_model import ModelMonster
-        # reset startTime so fit_runtime is just for fit
-        self.model.startTime = datetime.datetime.now()
         self.model = self.model.fit(
             df_train_transformed, future_regressor=future_regressor
         )
